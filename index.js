db639183fa305a49e6e58212016264d6866c941d
kmmh <mezba.marj@gmail.com>
2022-11-12 12:15:20 +0600

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 import './style.css';
 
 // Write Javascript code!
+
+console.log('hello');
+
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Lear and earn</h1>`;
